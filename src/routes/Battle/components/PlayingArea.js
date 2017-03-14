1aface128fa328c08f47c65c2e5027143470f523
Ross Benzie <rossbenzie@gmail.com>
2017-03-14 17:26:54 +0000

    Add three BattleRows to PlayingArea

--- src/routes/Battle/components/PlayingArea.js
+++ src/routes/Battle/components/PlayingArea.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { BattleRow } from 'routes/Battle/components/BattleRow'
 
 export const PlayingArea = (props) => (
   <div className="playing-area" style={{ margin: '0 auto' }} >
+
+  { <BattleRow />}
+  { <BattleRow />}
+  { <BattleRow />}
+
   </div>
 )
--- tests/routes/Battle/components/PlayingArea.spec.js
+++ tests/routes/Battle/components/PlayingArea.spec.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { bindActionCreators } from 'redux'
 import { PlayingArea } from 'routes/Battle/components/PlayingArea'
+import { BattleRow } from 'routes/Battle/components/BattleRow'
 import { shallow } from 'enzyme'
 
 describe('(Component) PlayingArea', () => {
@@ -12,6 +13,11 @@ describe('(Component) PlayingArea', () => {
 
   it('Should render as a <div>.', () => {
     expect(_wrapper.is('div')).to.equal(true)
+  })
+
+  it('Should render three BattleRows.', () => {
+    const battleRows = _wrapper.find(BattleRow);
+    expect(battleRows).to.have.length(3);
   })
 
 });
