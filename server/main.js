4079178c51d9575c1f3ccfa40849f0b769b10f58
bvjones <nanteos@hotmail.com>
2017-03-14 19:04:19 +0000

    refactor Facebook login. Tests WIP

--- server/main.js
+++ server/main.js
@@ -7,14 +7,16 @@ const webpack = require('webpack')
 const webpackConfig = require('../config/webpack.config')
 const project = require('../config/project.config')
 const compress = require('compression')
+const passport = require('passport')
+var FacebookStrategy = require('passport-facebook').Strategy
 
-//Import mongoDB
+// Import mongoDB
 const mongo = require('mongodb')
 const monk = require('monk')
-const mongoUrl = process.env.MONGO_URL;
-var db = monk(mongoUrl);
+const mongoUrl = process.env.MONGO_URL
+var db = monk(mongoUrl)
 
-//import session from express
+// import session from express
 const session = require('express-session')
 const MongodbStoreFactory = require('connect-mongodb-session')
 const MongoDBStore = MongodbStoreFactory(session)
@@ -24,12 +26,46 @@ const app = express()
 // Apply gzip compression
 app.use(compress())
 
+var FBStrategy = new FacebookStrategy({
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  profileFields: ['id', 'displayName', 'photos', 'email']
+},
+function (accessToken, refreshToken, profile, cb) {
+  var user = {
+    identifier: profile.id,
+    profile: profile.displayName
+  }
+  console.log(user)
+  console.log(arguments)
+  return cb(null, user)
+})
+
+passport.use(FBStrategy)
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user.identifier)
+})
+
+passport.deserializeUser(function (identifier, cb) {
+  // For this demo, we'll just return an object literal since our user
+  // objects are this trivial.  In the real world, you'd probably fetch
+  // your user object from your database here.
+  cb(null, {
+    identifier: identifier, facebookId: identifier.match(/\d+$/)[0]
+  })
+})
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 // Set up sessions
 var store = new MongoDBStore(
   {
     uri: mongoUrl,
     collection: 'mySessions'
-  });
+  })
 
 app.use(require('express-session')({
   secret: 'This is a secret',
@@ -39,12 +75,12 @@ app.use(require('express-session')({
   store: store,
   resave: true,
   saveUninitialized: true
-}));
+}))
 
-app.use(function(req,res,next){
-  req.db = db;
-  next();
-});
+app.use(function (req, res, next) {
+  req.db = db
+  next()
+})
 
 // ------------------------------------
 // Apply Webpack HMR Middleware
@@ -75,6 +111,17 @@ if (project.env === 'development') {
   // This rewrites all routes requests to the root /index.html file
   // (ignoring file requests). If you want to implement universal
   // rendering, you'll want to remove this middleware.
+
+  app.get('/auth/facebook',
+    passport.authenticate('facebook'))
+
+  app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function (req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/')
+    })
+
   app.use('*', function (req, res, next) {
     const filename = path.join(compiler.outputPath, 'index.html')
     compiler.outputFileSystem.readFile(filename, (err, result) => {
--- src/routes/SignIn/components/SignInForm.jsx
+++ src/routes/SignIn/components/SignInForm.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
-import FacebookLogin from 'react-facebook-login'
-
-const responseFacebook = (response) => {
-  console.log(response);
-}
 
 export const SignInForm = () => (
   <div>
-    <FacebookLogin
-      appId={process.env.APP_ID}
-      autoLoad={true}
-      fields='name,email,picture'
-      callback={responseFacebook} />
+    <a href='/auth/facebook' method='post'>Facebook Sign In</a>
+    <a>Hi {window.user}</a>
   </div>
 )
 
