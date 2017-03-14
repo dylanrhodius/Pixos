21ad98fd10012648f43e04c9e214fccbd79a6961
Ross Benzie <rossbenzie@gmail.com>
2017-03-14 16:49:57 +0000

    Add BattleRow

---
+++ src/routes/Battle/components/BattleRow.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export const BattleRow = (props) => (
+  <div className="battle-row" style={{ margin: '0 auto' }} >
+  </div>
+)
---
+++ tests/routes/Battle/components/BattleRow.spec.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { bindActionCreators } from 'redux'
+import { BattleRow } from 'routes/Battle/components/BattleRow'
+import { shallow } from 'enzyme'
+
+describe('(Component) BattleRow', () => {
+  let _props, _wrapper
+
+  beforeEach(() => {
+    _wrapper = shallow(<BattleRow {..._props} />)
+  })
+
+  it('Should render as a <div>.', () => {
+    expect(_wrapper.is('div')).to.equal(true)
+  })
+
+});
