1d6132321cc0cec22a977b87503fba1ead80eca7
Ross Benzie <rossbenzie@gmail.com>
2017-03-14 17:56:11 +0000

    Delete some redundant tests

--- src/routes/Battle/components/Battle.js
+++ src/routes/Battle/components/Battle.js
@@ -7,20 +7,8 @@ export const Battle = (props) => (
 
   { <InfoBar/> }
   { <Board/> }
-    <button className='btn btn-default' onClick={props.increment}>
-      Increment
-    </button>
-    {' '}
-    <button className='btn btn-default' onClick={props.doubleAsync}>
-      Double (Async)
-    </button>
+
   </div>
 )
-
-Battle.propTypes = {
-  battle     : React.PropTypes.number.isRequired,
-  doubleAsync : React.PropTypes.func.isRequired,
-  increment   : React.PropTypes.func.isRequired
-}
 
 export default Battle
--- tests/routes/Battle/components/Battle.spec.js
+++ tests/routes/Battle/components/Battle.spec.js
@@ -24,16 +24,6 @@ describe('(Component) Battle', () => {
     expect(_wrapper.is('div')).to.equal(true)
   })
 
-  // it('Should render with an <h2> that includes Sample Battle text.', () => {
-  //   expect(_wrapper.find('h2').text()).to.match(/Battle:/)
-  // })
-
-  // it('Should render props.battle at the end of the sample battle <h2>.', () => {
-  //   expect(_wrapper.find('h2').text()).to.match(/5$/)
-  //   _wrapper.setProps({ battle: 8 })
-  //   expect(_wrapper.find('h2').text()).to.match(/8$/)
-  // })
-
   it('Should render an InfoBar.', () => {
     const infobar = _wrapper.find(InfoBar);
     expect(infobar).to.exist;
@@ -43,45 +33,5 @@ describe('(Component) Battle', () => {
     expect(board).to.exist;
   })
 
-  xdescribe('An increment button...', () => {
-    let _button
 
-    beforeEach(() => {
-      _button = _wrapper.find('button').filterWhere(a => a.text() === 'Increment')
-    })
-
-    it('has bootstrap classes', () => {
-      expect(_button.hasClass('btn btn-default')).to.be.true
-    })
-
-    it('Should dispatch a `increment` action when clicked', () => {
-      _spies.dispatch.should.have.not.been.called
-
-      _button.simulate('click')
-
-      _spies.dispatch.should.have.been.called
-      _spies.increment.should.have.been.called
-    })
-  })
-
-  xdescribe('A Double (Async) button...', () => {
-    let _button
-
-    beforeEach(() => {
-      _button = _wrapper.find('button').filterWhere(a => a.text() === 'Double (Async)')
-    })
-
-    it('has bootstrap classes', () => {
-      expect(_button.hasClass('btn btn-default')).to.be.true
-    })
-
-    it('Should dispatch a `doubleAsync` action when clicked', () => {
-      _spies.dispatch.should.have.not.been.called
-
-      _button.simulate('click')
-
-      _spies.dispatch.should.have.been.called
-      _spies.doubleAsync.should.have.been.called
-    })
-  })
 })
