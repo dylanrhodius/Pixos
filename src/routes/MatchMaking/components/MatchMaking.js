adafefd08d9f6ba240ef9c1f664c74652781763c
Rick Clark <rkingsleyclark@gmail.com>
2017-03-16 15:27:19 +0000

    Add init:battle event listener to Matchmaking
    Redirect browser to /battle on init:battle

--- src/routes/MatchMaking/components/MatchMaking.js
+++ src/routes/MatchMaking/components/MatchMaking.js
@@ -3,11 +3,18 @@ import React from 'react'
 import io from 'socket.io-client';
 const socket = io.connect(`${window.location.origin}`);
 
+socket.on("init:battle", function() {
+  console.log("battle initiated");
+  window.location.href = '/battle'
+})
+
 export default class MatchMaking extends React.Component {
 
   componentDidMount() {
     socket.emit('request:matchmaking');
   }
+
+
 
   render() {
     return (
