4bac5844bd090525ef104357121228e024e68150
Rob Holden <robholdenflag@gmail.com>
2017-03-17 17:28:39 +0000

    Cost added to array of dinos

--- src/routes/Battle/components/Card.js
+++ src/routes/Battle/components/Card.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export const Card = (props) => (
-
   <div className="card-item mx-1 my-2">
     <img className="card-img-fluid" src={props.imgUrl} alt="Responsive image"/>
       <h4 className="card-title">{ props.name }</h4>
