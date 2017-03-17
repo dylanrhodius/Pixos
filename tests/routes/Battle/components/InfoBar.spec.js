06c1e96c22fde5c3ffe27fc8f2ac6fdc0765295e
Mike Field-May <mfieldmay@gmail.com>
2017-03-17 13:09:39 +0000

    Fix failing InfoBar test

--- tests/routes/Battle/components/InfoBar.spec.js
+++ tests/routes/Battle/components/InfoBar.spec.js
@@ -2,11 +2,16 @@ import React from 'react'
 import { bindActionCreators } from 'redux'
 import InfoBar from 'routes/Battle/components/InfoBar'
 import { shallow } from 'enzyme'
+import { TEST_STATE } from './BattleStateHelper'
+
 
 describe('(Component) InfoBar', () => {
   let _props, _wrapper
 
   beforeEach(() => {
+    _props = {
+      battle: TEST_STATE
+    }
     _wrapper = shallow(<InfoBar {..._props} />)
   })
 
