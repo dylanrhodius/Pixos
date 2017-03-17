e05353d74ea89ff16af299f32141f54569012677
bvjones <nanteos@hotmail.com>
2017-03-17 14:40:00 +0000

    Rendered deck of cards on Deckbuilder route

--- src/routes/DeckBuilder/components/DeckBuilder.js
+++ src/routes/DeckBuilder/components/DeckBuilder.js
@@ -1,19 +1,26 @@
 import React from 'react'
-
+var cardData = require('../../../../server/cardData').CARD_DATA
+import Card from '../../Battle/components/Card'
 
 export default class DeckBuilder extends React.Component {
 
-  componentDidMount() {
-
+  componentDidMount () {
   }
 
+  renderDeckComponents () {
+    console.log(cardData)
+    return cardData.map(
+      (card, i) => <Card key={i} {...card} />
+    )
+  }
 
-
-  render() {
+  render () {
+    let cards = this.renderDeckComponents()
     return (
       <div>
         <h2>DeckBuilder</h2>
+        { cards }
       </div>
-    );
+    )
   }
 }
