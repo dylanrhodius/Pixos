050c642fa3bca5042593e7a3263446a8486ca5eb
Rick Clark <rkingsleyclark@gmail.com>
2017-03-19 10:54:08 +0000

    Shorten max width of game card name

--- src/routes/Battle/components/Card.js
+++ src/routes/Battle/components/Card.js
@@ -13,14 +13,14 @@ render() {
 
       <div className={`game-card box-shadow mx-1 ${this.props.type}-faint-bkgrnd d-flex align-items-stretch`}>
         <div className={`game-card-name-holder pb-1 ${this.props.type}-main-bkgrnd`}>
-          <h4 className="card-title game-card-name m-0 text-left highlighted-white-text">
+          <h4 className="game-card-name m-0 text-left highlighted-white-text">
             { this.props.name.charAt(0).toUpperCase() + this.props.name.slice(1) }
           </h4>
         </div>
         <div className="d-flex justify-content-center align-items-start game-card-img-holder p-1">
           <img className="game-card-img" src={this.props.imgUrl} alt="Card image cap"/>
         </div>
-        <span className={`game-card-text ${this.props.type}-bkgrnd circle d-inline-block mx-auto highlighted-white-text`}>
+        <span className={`game-card-power ${this.props.type}-bkgrnd circle d-inline-block mx-auto highlighted-white-text`}>
           { this.props.power }
         </span>
       </div>
