ad7c557491bbca65dac342f012393b20c2c94b10
bvjones <nanteos@hotmail.com>
2017-03-20 16:30:04 +0000

    Render button on the signIn page

--- src/routes/SignIn/components/SignInPage.jsx
+++ src/routes/SignIn/components/SignInPage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import SignInForm from './SignInForm.jsx'
 import { Link } from 'react-router'
 import Header from '../../../components/Header'
+import RaisedButton from 'material-ui/RaisedButton'
+
 
 export default class SignInPage extends React.Component {
 
@@ -9,12 +11,12 @@ export default class SignInPage extends React.Component {
     return (
       <div>
         <Header />
-        <h2>Sign In!</h2>
         {<SignInForm />}
-        <Link to={`/battle`} >Start Matchmaking</Link>
-        <br />
-        <Link to={`/deckbuilder`} >Deck Builder</Link>
-
+        <br/>
+        <Link to={`/battle`} ><RaisedButton label="Start Matchmaking" primary={true}/></Link>
+        <br/>
+        <br/>
+        <Link to={`/deckbuilder`} ><RaisedButton label="Deck Builder" primary={true}/></Link>
       </div>
     );
   }
