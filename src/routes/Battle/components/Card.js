06f6285c78c5e371ed31acdcb7c7dc03df061ac3
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-03-21 17:17:55 +0000

    Display tooltip information on Battle cards

--- src/routes/Battle/components/Card.js
+++ src/routes/Battle/components/Card.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Card.scss'
+import ReactTooltip from 'react-tooltip'
+
 
 export default class Card extends React.Component {
 
@@ -15,12 +17,16 @@ getSpecialIcon() {
   }
 }
 
+capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
 render() {
   let specialIcon = this.getSpecialIcon()
   let showPointerClass
   if (this.props.showPointer) { showPointerClass = "show-pointer" }
   return (
-      <div className={`game-card box-shadow mx-1 ${this.props.type}-faint-bkgrnd d-flex align-items-stretch ${showPointerClass}`}>
+      <div className={`game-card box-shadow mx-1 ${this.props.type}-faint-bkgrnd d-flex align-items-stretch ${showPointerClass}`} data-tip data-for={`card_${this.props.name}`}>
         <div className={`game-card-name-holder pb-1 ${this.props.type}-main-bkgrnd`}>
           <h4 className="game-card-name m-0 text-left highlighted-white-text">
             { this.props.name.charAt(0).toUpperCase() + this.props.name.slice(1) }
@@ -32,6 +38,10 @@ render() {
         <span className={`game-card-power ${this.props.type}-bkgrnd circle d-inline-block mx-auto highlighted-white-text`}>
           { this.props.power }
         </span>
+        <ReactTooltip id={`card_${this.props.name}`}>
+          <p>{this.capitalize(this.props.name)}</p>
+          <p>{this.props.description}</p>
+        </ReactTooltip>
         { specialIcon }
       </div>
 
