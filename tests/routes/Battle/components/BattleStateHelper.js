b352c858f424d60c147931cc80aefbf329ea9cb9
Ross Benzie <rossbenzie@gmail.com>
2017-03-22 14:27:33 +0000

    Add more tests

--- tests/routes/Battle/components/BattleStateHelper.js
+++ tests/routes/Battle/components/BattleStateHelper.js
@@ -1,28 +1,61 @@
 export const TEST_STATE = {
   self: {
     myTurn: false,
+    readyForNewRound: false,
+    roundCounter: 1,
+    PlayerNotification: "",
+    gameEnded: false,
     name: '',
+    img: '',
     power: 0,
     score: 0,
     hasPassed: false,
     hand: [],
+    discardPile: [],
     playingArea: {
       land: [],
       water: [],
       air: []
+    },
+    meteor: {
+      land: false,
+      water: false,
+      air: false
+    },
+    paragon: {
+      land: false,
+      water: false,
+      air: false
     }
   },
   enemy: {
     myTurn: false,
+    readyForNewRound: false,
+    roundCounter: 1,
+    PlayerNotification: "",
+    gameEnded: false,
     name: '',
+    img: '',
     power: 0,
     score: 0,
     hasPassed: false,
     hand: [],
+    discardPile: [],
     playingArea: {
       land: [],
       water: [],
       air: []
+    },
+    meteor: {
+      land: false,
+      water: false,
+      air: false
+    },
+    paragon: {
+      land: false,
+      water: false,
+      air: false
     }
-  }
+  },
+  turnFinished: false
 }
