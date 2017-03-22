8d97eccad4e9625ea21ef7e344080a123b1b29e6
Rick Clark <rkingsleyclark@gmail.com>
2017-03-22 01:02:05 +0000

    Add glow to current player's hand and avatar

--- src/routes/Battle/components/InfoBar.js
+++ src/routes/Battle/components/InfoBar.js
@@ -37,10 +37,11 @@ export default class InfoBar extends React.Component {
 
   render () {
     let passButton = this.loadContent()
+    let myTurn = this.props.battle.self.myTurn
     return (
       <div className="info-bar col-1 d-flex flex-column justify-content-center align-items-center grey-bkgrnd">
         <PixosMenu />
-        <img className="game-avatar circle circle-highlight mb-1" src={`${this.props.battle.enemy.img}`}/>
+        <img className={`game-avatar ${!myTurn ? 'pulse' : ''} circle circle-highlight mb-1`} src={`${this.props.battle.enemy.img}`}/>
         <span className="info-bar-text mx-2">{ this.props.battle.enemy.name }</span>
         <span className="my-2 info-bar-text land-main-bkgrnd circle circle-highlight info-bar-indicator">{this.props.battle.enemy.score}</span>
         <span className="info-bar-text small-text mb-auto">Score</span>
@@ -58,7 +59,7 @@ export default class InfoBar extends React.Component {
         <span className="mt-auto mb-1 info-bar-text small-text">Score</span>
         <span className="mb-2 info-bar-text land-main-bkgrnd circle circle-highlight info-bar-indicator">{this.props.battle.self.score}</span>
         <span className="info-bar-text mx-2">{ this.props.battle.self.name }</span>
-        <img className="game-avatar circle circle-highlight mb-2 mt-1" src={`${this.props.battle.self.img}`}/>
+        <img className={`game-avatar ${myTurn ? 'pulse' : ''} circle circle-highlight mb-2 mt-1`} src={`${this.props.battle.self.img}`}/>
         {passButton}
     </div>
     )
