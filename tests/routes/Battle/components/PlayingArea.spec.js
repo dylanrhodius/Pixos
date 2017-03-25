87de302f3ef6270a347ce3841a5f2f94d7f45fec
Rick Clark <rkingsleyclark@gmail.com>
2017-03-25 22:05:26 +0000

    Fix failing PlayingArea test

--- tests/routes/Battle/components/PlayingArea.spec.js
+++ tests/routes/Battle/components/PlayingArea.spec.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { bindActionCreators } from 'redux'
-import { PlayingArea } from 'routes/Battle/components/PlayingArea'
-import { BattleRow } from 'routes/Battle/components/BattleRow'
+import PlayingArea from 'routes/Battle/components/PlayingArea'
+import BattleRow from 'routes/Battle/components/BattleRow'
 import { shallow } from 'enzyme'
 
-xdescribe('(Component) PlayingArea', () => {
+describe('(Component) PlayingArea', () => {
   let _props, _wrapper
+  _props = { type: 'land',
+             passed: false,
+             cardArrays: {
+               land: [],
+               air: [],
+               water: []
+             } }
 
   beforeEach(() => {
     _wrapper = shallow(<PlayingArea {..._props} />)
